5a613660b2a80f949908d4d4ef3592e7afa7c87e
Baekwoo Chung <baekwoo@gmail.com>
2015-06-15 06:11:12 +0900

    unity version change other file

--- Assets/GrowthbeatComponent.cs
+++ Assets/GrowthbeatComponent.cs
@@ -5,11 +5,10 @@ public class GrowthbeatComponent : MonoBehaviour {
 	
 	void Awake () {
 		Growthbeat.GetInstance().Initialize("OrUq6yu3KsZ1MjJ7", "saWAVZs5f531VXk3ZVgJZwK1vQUzPg23");
-		Growthbeat.GetInstance().Start();
 	}
 	
 	void Start () {
-		
+		Growthbeat.GetInstance().Start();
 	}
 	
 	void Update () {
