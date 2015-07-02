898631ec3c9163f134876b159f8889380bf4656d
y.uchiyama <yosuke.u0710@gmail.com>
2015-07-03 06:32:08 +0900

    Add Sample code

--- sample/Assets/GrowthbeatComponent.cs
+++ sample/Assets/GrowthbeatComponent.cs
@@ -8,29 +8,32 @@
 
 using UnityEngine;
 using System.Collections;
+using NotificationServices = UnityEngine.iOS.NotificationServices;
 
 public class GrowthbeatComponent : MonoBehaviour
 {
-	
+	bool tokenSent = false;
+
 	void Awake ()
 	{
 		Growthbeat.GetInstance ().Initialize ("OrUq6yu3KsZ1MjJ7", "saWAVZs5f531VXk3ZVgJZwK1vQUzPg23", "955057365401", true);
+		GrowthPush.GetInstance ().RequestDeviceToken ();  
 	}
 	
 	void Start ()
 	{
 		//GrowthBeat
 		Growthbeat.GetInstance ().Start ();
-		Growthbeat.GetInstance ().Stop ();
-		Growthbeat.GetInstance ().SetLoggerSilent (true);
+		Growthbeat.GetInstance ().Stop (); 
 
 		//GrowthAnalytics
 		GrowthAnalytics.GetInstance ().Open ();
+		GrowthAnalytics.GetInstance ().SetBasicTags ();
 		GrowthAnalytics.GetInstance ().Tag ("TagTest");
 		GrowthAnalytics.GetInstance ().Tag ("TagTest_Number", "1234");
 		GrowthAnalytics.GetInstance ().Track ("TrackEvent");
 		GrowthAnalytics.GetInstance ().Purchase (1234, "Category", "product");
-		GrowthAnalytics.GetInstance ().SetUserId ("UserId");
+		GrowthAnalytics.GetInstance (). SetUserId ("UserId");
 		GrowthAnalytics.GetInstance ().SetName ("Name");
 		GrowthAnalytics.GetInstance ().SetAge (1234);
 		GrowthAnalytics.GetInstance ().SetGender (GrowthAnalytics.Gender.GenderFemale);
@@ -40,11 +43,24 @@ public class GrowthbeatComponent : MonoBehaviour
 
 		//GrowthPush
 		GrowthPush.GetInstance ().ClearBadge ();
+		GrowthPush.GetInstance ().SetTag ("TagTest");
+		GrowthPush.GetInstance ().SetTag ("userId", "123");
+		GrowthPush.GetInstance ().TrackEvent ("Launch");
+		GrowthPush.GetInstance ().TrackEvent ("Purchace", "100");
+		GrowthPush.GetInstance ().setDeviceTags ();
 
 	}
 	
 	void Update ()
 	{
-		
+	#if UNITY_IPHONE
+		if (!tokenSent) {
+			byte[] token = NotificationServices.deviceToken;
+			if (token != null) {
+				GrowthPush.GetInstance ().SetDeviceToken(System.BitConverter.ToString(token));
+			}
+		}
+	#endif
 	}
 }
+  
