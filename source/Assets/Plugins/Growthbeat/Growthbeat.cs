35c9aa1de5bc687f6fabe9f9760c9ea5e6da9bf0
Ogawa Shigeru <ogawa@sirok.co.jp>
2015-09-28 21:24:33 +0900

    fix Growthbeat Interface

--- source/Assets/Plugins/Growthbeat/Growthbeat.cs
+++ source/Assets/Plugins/Growthbeat/Growthbeat.cs
@@ -15,14 +15,14 @@ using System.Runtime.InteropServices;
 public class Growthbeat
 {
 	private static Growthbeat instance = new Growthbeat ();
-	
+
 	#if UNITY_IPHONE
-	[DllImport("__Internal")] static extern void growthbeatInitializeWithApplicationId(string applicationId, string credentialId);
-	[DllImport("__Internal")] static extern void growthbeatStart();
-	[DllImport("__Internal")] static extern void growthbeatStop();
-	[DllImport("__Internal")] static extern void growthbeatSetLoggerSilent(bool silent);
+	[DllImport("__Internal")] static extern void initializeWithApplicationId(string applicationId, string credentialId);
+	[DllImport("__Internal")] static extern void start();
+	[DllImport("__Internal")] static extern void stop();
+	[DllImport("__Internal")] static extern void setLoggerSilent(bool silent);
 	#endif
-	
+
 	public static Growthbeat GetInstance ()
 	{
 		return Growthbeat.instance;
@@ -31,25 +31,25 @@ public class Growthbeat
 	#if UNITY_ANDROID
 	private static AndroidJavaObject growthbeat;
 	#endif
-	
+
 	private Growthbeat() {
 		#if UNITY_ANDROID
 		using(AndroidJavaClass gbcclass = new AndroidJavaClass( "com.growthbeat.Growthbeat" )) {
-			growthbeat = gbcclass.CallStatic<AndroidJavaObject>("getInstance"); 
+			growthbeat = gbcclass.CallStatic<AndroidJavaObject>("getInstance");
 		}
 		#endif
 	}
-	
+
 	public void Initialize (string applicationId, string credentialId, bool debug)
 	{
 		#if UNITY_ANDROID
 		if (growthbeat == null)
 			return;
-		AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"); 
-		AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"); 
-		growthbeat.Call("initialize", activity, applicationId, credentialId, debug);
+		AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+		growthbeat.Call("initialize", activity, applicationId, credentialId);
 		#elif UNITY_IPHONE && !UNITY_EDITOR
-		growthbeatInitializeWithApplicationId(applicationId, credentialId);
+		initializeWithApplicationId(applicationId, credentialId);
 		#endif
 	}
 
@@ -60,7 +60,7 @@ public class Growthbeat
 			return;
 		growthbeat.Call("start");
 		#elif UNITY_IPHONE && !UNITY_EDITOR
-		growthbeatStart();
+		start();
 		#endif
 	}
 
@@ -71,7 +71,7 @@ public class Growthbeat
 			return;
 		growthbeat.Call("stop");
 		#elif UNITY_IPHONE && !UNITY_EDITOR
-		growthbeatStop();
+		stop();
 		#endif
 	}
 
@@ -82,8 +82,8 @@ public class Growthbeat
 			return;
 		growthbeat.Call("setLoggerSilent", silent);
 		#elif UNITY_IPHONE && !UNITY_EDITOR
-		growthbeatSetLoggerSilent(silent);
+		setLoggerSilent(silent);
 		#endif
 	}
-	
+
 }
--- source/Assets/Plugins/iOS/Growthbeat.mm
+++ source/Assets/Plugins/iOS/Growthbeat.mm
@@ -9,19 +9,19 @@
 #import <UIKit/UIKit.h>
 #import <Growthbeat/Growthbeat.h>
 
-extern "C" void growthbeatInitializeWithApplicationId(const char* applicationId, const char* credentialId) {
+extern "C" void initializeWithApplicationId(const char* applicationId, const char* credentialId) {
     [[Growthbeat sharedInstance] initializeWithApplicationId:[NSString stringWithCString:applicationId encoding:NSUTF8StringEncoding]
     											credentialId:[NSString stringWithCString:credentialId encoding:NSUTF8StringEncoding]];
 }
 
-extern "C" void growthbeatStart() {
+extern "C" void start() {
     [[Growthbeat sharedInstance] start];
 }
 
-extern "C" void growthbeatStop() {
+extern "C" void stop() {
 	[[Growthbeat sharedInstance] stop];
 }
 
-extern "C" void growthbeatSetLoggerSilent(bool silent) {
-	[[Growthbeat sharedInstance] setLoggerSilent:silent];	
+extern "C" void setLoggerSilent(bool silent) {
+	[[Growthbeat sharedInstance] setLoggerSilent:silent];
 }
