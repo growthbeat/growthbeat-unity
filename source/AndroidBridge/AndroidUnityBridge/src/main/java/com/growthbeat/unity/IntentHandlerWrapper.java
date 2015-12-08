17f39d44e4cb1cfeed8bcb01ff163bd6974d8134
Ogawa Shigeru <ogawa@sirok.co.jp>
2015-12-08 17:55:52 +0900

    change project

---
+++ source/AndroidBridge/AndroidUnityBridge/src/main/java/com/growthbeat/unity/IntentHandlerWrapper.java
@@ -0,0 +1,67 @@
+package com.growthbeat.unity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.growthbeat.GrowthbeatCore;
+import com.growthbeat.intenthandler.IntentHandler;
+import com.growthbeat.intenthandler.NoopIntentHandler;
+import com.growthbeat.intenthandler.UrlIntentHandler;
+import com.growthbeat.model.CustomIntent;
+import com.unity3d.player.UnityPlayer;
+
+import android.content.Intent;
+import android.util.Log;
+
+public class IntentHandlerWrapper {
+
+    private  List<IntentHandler> intentHandlers = new ArrayList<IntentHandler>();
+
+    private static final IntentHandlerWrapper instance = new IntentHandlerWrapper();
+
+    public IntentHandlerWrapper() {
+        super();
+        GrowthbeatCore.getInstance().setIntentHandlers(intentHandlers);
+    }
+
+    private static IntentHandlerWrapper getInstance() {
+        return IntentHandlerWrapper.instance;
+    }
+
+    public void clearIntentHandlers() {
+        intentHandlers.clear();
+    }
+
+    public void addNoopIntentHandler() {
+        intentHandlers.add(new NoopIntentHandler());
+    }
+
+    public void addUrlIntentHandler() {
+        intentHandlers.add(new UrlIntentHandler(GrowthbeatCore.getInstance().getContext()));
+    }
+
+    public void addCustomIntentHandler(final String gameObject) {
+        intentHandlers.add(new IntentHandler() {
+            public boolean handle(com.growthbeat.model.Intent intent) {
+                if (intent.getType() != com.growthbeat.model.Intent.Type.custom)
+                    return false;;
+                Map<String, String> extra = ((CustomIntent) intent).getExtra();
+                JSONObject json = new JSONObject();
+                for (Map.Entry<String, String> entry : extra.entrySet()) {
+                    try {
+                        json.put(entry.getKey(), entry.getValue());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                UnityPlayer.UnitySendMessage(gameObject, "handleCustomIntent", json.toString());
+                return true;
+            }
+        });
+    }
+
+}
---
+++ source/AndroidBridge/AndroidUnityBridge/src/main/java/com/growthbeat/unity/IntentReceiveActivity.java
@@ -0,0 +1,29 @@
+package com.growthbeat.unity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.growthbeat.link.GrowthLink;
+import com.unity3d.player.UnityPlayerProxyActivity;
+
+public class IntentReceiveActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Uri uri = getIntent().getData();
+        GrowthLink.getInstance().handleOpenUrl(uri);
+        Intent intent = new Intent(this, UnityPlayerProxyActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        // nothing to do when button pressed.
+    }
+
+}
