41270ee7b18738685a6bf31d2d9c91bee52fe4c8
Ogawa Shigeru <ogawa@sirok.co.jp>
2015-12-09 19:24:49 +0900

    fix intentHandler

--- plugin/Assets/Growthbeat/IntentHandler.cs
+++ plugin/Assets/Growthbeat/IntentHandler.cs
@@ -5,87 +5,91 @@ using System.Collections.Generic;
 
 using System.Runtime.InteropServices;
 
-public class IntentHandler
-{
+namespace Growthbeat {
 
-	#if UNITY_IPHONE
-	[DllImport("__Internal")] static extern void initializeIntentHandlers();
-	[DllImport("__Internal")] static extern void addNoopIntentHandler();
-	[DllImport("__Internal")] static extern void addUrlIntentHandler();
-	[DllImport("__Internal")] static extern void addCustomIntentHandler(string gameObjectName, string methodName);
-	#endif
-
-	#if UNITY_ANDROID && !UNITY_EDITOR
-	private AndroidJavaObject intentHandler;
-
-	private AndroidJavaObject javaObject;
-
-	private void RunBlockOnThread(Action runBlock) {
-		
-		using(AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer")){
-			var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-			activity.Call("runOnUiThread", new AndroidJavaRunnable(runBlock));	
-		}
-
-	}
-	#endif
-
-	private IntentHandler()
+	public class IntentHandler
 	{
+
+		#if UNITY_IPHONE
+		[DllImport("__Internal")] static extern void gb_initializeIntentHandlers();
+		[DllImport("__Internal")] static extern void gb_addNoopIntentHandler();
+		[DllImport("__Internal")] static extern void gb_addUrlIntentHandler();
+		[DllImport("__Internal")] static extern void gb_addCustomIntentHandler(string gameObjectName, string methodName);
+		#endif
+
 		#if UNITY_ANDROID && !UNITY_EDITOR
-		using(AndroidJavaClass intentHandlerclass = new AndroidJavaClass( "com.growthbeat.intenthandler.IntentHandlerWrapper" ))
-		{
-			intentHandler = intentHandlerclass.CallStatic<AndroidJavaObject>("getInstance");
+		private AndroidJavaObject intentHandler;
+
+		private void RunBlockOnThread(Action runBlock) {
+			
+			using(AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer")){
+				var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+				activity.Call("runOnUiThread", new AndroidJavaRunnable(runBlock));	
+			}
+
 		}
 		#endif
+
+		private IntentHandler()
+		{
+			#if UNITY_ANDROID && !UNITY_EDITOR
+			using(AndroidJavaClass intentHandlerclass = new AndroidJavaClass( "com.growthbeat.intenthandler.IntentHandlerWrapper" ))
+			{
+				intentHandler = intentHandlerclass.CallStatic<AndroidJavaObject>("getInstance");
+			}
+			#elif UNITY_IPHONE && !UNITY_EDITOR
+			gb_initializeIntentHandlers();
+			#endif
+		}
+
+		private static IntentHandler instance = new IntentHandler();
+
+		public static IntentHandler GetInstance ()
+		{
+			return IntentHandler.instance;
+		}
+
+		public void ClearIntentHandlers()  {
+			#if UNITY_ANDROID
+			RunBlockOnThread(() => {
+				intentHandler.Call("clearIntentHandlers");
+			});
+			#elif UNITY_IPHONE && !UNITY_EDITOR
+			gb_clearIntentHandlers();
+			#endif
+
+		}
+
+		public void AddNoopIntentHandler()  {
+			#if UNITY_ANDROID
+			RunBlockOnThread(() => {
+				intentHandler.Call("addNoopIntentHandler");
+			});
+			#elif UNITY_IPHONE && !UNITY_EDITOR
+			gb_addNoopIntentHandler();
+			#endif
+		}
+
+		public void AddUrlIntentHandler()  {
+			#if UNITY_ANDROID
+			RunBlockOnThread(() => {
+				intentHandler.Call("addUrlIntentHandler");
+			});
+			#elif UNITY_IPHONE && !UNITY_EDITOR
+			gb_addUrlIntentHandler();
+			#endif
+		}
+
+		public void AddCustomIntentHandler(string gameObjectName, string methodName)  {
+			#if UNITY_ANDROID
+			RunBlockOnThread(() => {
+				intentHandler.Call("addCustomIntentHandler", gameObjectName, methodName);
+			});
+			#elif UNITY_IPHONE && !UNITY_EDITOR
+			gb_addCustomIntentHandler(gameObjectName, methodName);
+			#endif
+		}
+		
 	}
 
-	private static IntentHandler instance = new IntentHandler();
-
-	public static IntentHandler GetInstance ()
-	{
-		return IntentHandler.instance;
-	}
-
-	public void ClearIntentHandlers()  {
-		#if UNITY_ANDROID
-		RunBlockOnThread(() => {
-			intentHandler.Call("clearIntentHandlers");
-		});
-		#elif UNITY_IPHONE && !UNITY_EDITOR
-		initializeIntentHandlers();
-		#endif
-
-	}
-
-	public void AddNoopIntentHandler()  {
-		#if UNITY_ANDROID
-		RunBlockOnThread(() => {
-			intentHandler.Call("addNoopIntentHandler");
-		});
-		#elif UNITY_IPHONE && !UNITY_EDITOR
-		addNoopIntentHandler();
-		#endif
-	}
-
-	public void AddUrlIntentHandler()  {
-		#if UNITY_ANDROID
-		RunBlockOnThread(() => {
-			intentHandler.Call("addUrlIntentHandler");
-		});
-		#elif UNITY_IPHONE && !UNITY_EDITOR
-		addUrlIntentHandler();
-		#endif
-	}
-
-	public void AddCustomIntentHandler(string gameObjectName, string methodName)  {
-		#if UNITY_ANDROID
-		RunBlockOnThread(() => {
-			intentHandler.Call("addCustomIntentHandler", gameObjectName, methodName);
-		});
-		#elif UNITY_IPHONE && !UNITY_EDITOR
-		addCustomIntentHandler(gameObjectName, methodName);
-		#endif
-	}
-	
 }
--- plugin/Assets/Plugin/iOS/IntentHandlerWrapper.mm
+++ plugin/Assets/Plugin/iOS/IntentHandlerWrapper.mm
@@ -4,37 +4,44 @@
 #import <Growthbeat/GBUrlIntentHandler.h>
 #import <Growthbeat/GBNoopIntentHandler.h>
 
+extern "C"  {
 
-extern "C" void initializeIntentHandlers() {
-    [GrowthbeatCore sharedInstance].intentHandlers = [NSMutableArray array];
-}
+    void gb_initializeIntentHandlers() {
+        [[GrowthbeatCore sharedInstance] intentHandlers] = [NSMutableArray array];
+    }
 
-extern "C" void addNoopIntentHandler() {
-    NSMutableArray *mutableArray = [[GrowthbeatCore sharedInstance].intentHandlers mutableCopy];
-    [mutableArray addObject:[[GBNoopIntentHandler alloc] init]];
-    [GrowthbeatCore sharedInstance].intentHandlers = mutableArray;
-}
+    void gb_clearIntentHandlers() {
+        [[[GrowthbeatCore sharedInstance] intentHandlers] removeAllObjects];
+    }
 
-extern "C" void addUrlIntentHandler() {
-    NSMutableArray *mutableArray = [[GrowthbeatCore sharedInstance].intentHandlers mutableCopy];
-    [mutableArray addObject:[[GBUrlIntentHandler alloc] init]];
-    [GrowthbeatCore sharedInstance].intentHandlers = mutableArray;
-}
+    void gb_addNoopIntentHandler() {
+        NSMutableArray *mutableArray = [[GrowthbeatCore sharedInstance].intentHandlers mutableCopy];
+        [mutableArray addObject:[[GBNoopIntentHandler alloc] init]];
+        [[GrowthbeatCore sharedInstance] intentHandlers] = mutableArray;
+    }
 
-extern "C" void addCustomIntentHandler(const char* gameObject, const char* methodName) {
-    
-    if(gameObject == NULL)
-        return;
+    void gb_addUrlIntentHandler() {
+        NSMutableArray *mutableArray = [[GrowthbeatCore sharedInstance].intentHandlers mutableCopy];
+        [mutableArray addObject:[[GBUrlIntentHandler alloc] init]];
+        [[GrowthbeatCore sharedInstance] intentHandlers] = mutableArray;
+    }
 
-    NSMutableArray *mutableArray = [[GrowthbeatCore sharedInstance].intentHandlers mutableCopy];
-    [mutableArray addObject:[[GBCustomIntentHandler alloc] initWithBlock:^BOOL (GBCustomIntent *customIntent){
-        NSDictionary *dict = customIntent.extra;
-        NSError *err;
-        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict options:0 error:&err];
-        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-        UnitySendMessage([[NSString stringWithCString:gameObject encoding:NSUTF8StringEncoding] UTF8String], methodName, (char *)[jsonString UTF8String]);
-        return YES;
-    }]];
-    [GrowthbeatCore sharedInstance].intentHandlers = mutableArray;
-    
+    void gb_addCustomIntentHandler(const char* gameObject, const char* methodName) {
+        
+        if(gameObject == NULL)
+            return;
+
+        NSMutableArray *mutableArray = [[GrowthbeatCore sharedInstance].intentHandlers mutableCopy];
+        [mutableArray addObject:[[GBCustomIntentHandler alloc] initWithBlock:^BOOL (GBCustomIntent *customIntent){
+            NSDictionary *dict = customIntent.extra;
+            NSError *err;
+            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict options:0 error:&err];
+            NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+            UnitySendMessage([[NSString stringWithCString:gameObject encoding:NSUTF8StringEncoding] UTF8String], methodName, (char *)[jsonString UTF8String]);
+            return YES;
+        }]];
+        [[GrowthbeatCore sharedInstance] intentHandlers] = mutableArray;
+        
+    }
+
 }
