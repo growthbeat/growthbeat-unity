2862f88bc1228e58220957f3e8e59eeeb6e9f3c9
Ogawa Shigeru <ogawa@sirok.co.jp>
2016-05-31 14:55:11 +0900

    update Growthbeat iOS

---
+++ plugin/Assets/Plugins/iOS/GrowthLinkPlugin.mm
@@ -0,0 +1,50 @@
+//
+//  GrowthLinkPlugin.mm
+//
+
+#include "GrowthLinkPlugin.h"
+#import <Growthbeat/GrowthLink.h>
+
+static GrowthLinkPlugin *_instance = [GrowthLinkPlugin sharedInstance];
+
+@implementation GrowthLinkPlugin
+
+#pragma mark Object Initialization
+
++ (GrowthLinkPlugin *)sharedInstance
+{
+	return _instance;
+}
+
++ (void)initialize
+{
+	if (!_instance) {
+		_instance = [[GrowthLinkPlugin alloc] init];
+	}
+}
+
+- (id)init
+{
+	if (_instance != nil) {
+		return _instance;
+	}
+
+	if ((self = [super init])) {
+		_instance = self;
+		UnityRegisterAppDelegateListener(self);
+	}
+	return self;
+}
+
+#pragma mark - App (Delegate) Lifecycle
+
+- (void)onOpenURL:(NSNotification *)notification
+{
+	NSURL *url = notification.userInfo[@"url"];
+
+	if ([GrowthLink instancesRespondToSelector:@selector(handleOpenUrl:)]) {
+		[[GrowthLink sharedInstance] handleOpenUrl: url];
+	}
+}
+
+@end
