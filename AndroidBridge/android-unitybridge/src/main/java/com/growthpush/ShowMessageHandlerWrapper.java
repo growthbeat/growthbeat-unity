96b141ea69ba372a53d1841bd8fd8120ae2f144e
Shigeru Ogawa <mugi65.as@gmail.com>
2016-07-04 18:35:11 +0900

    build android wrapper

---
+++ AndroidBridge/android-unitybridge/src/main/java/com/growthpush/ShowMessageHandlerWrapper.java
@@ -0,0 +1,37 @@
+package com.growthpush;
+
+import com.growthbeat.message.handler.ShowMessageHandler;
+import com.unity3d.player.UnityPlayer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class ShowMessageHandlerWrapper {
+
+    private static Map<String, ShowMessageHandler.MessageRenderHandler> showMessageHandlers = new HashMap<>();
+
+    public static void trackEvent(final String name, final String value, final String gameObject, final String methodName) {
+
+        GrowthPush.getInstance().trackEvent(name, value, new ShowMessageHandler() {
+            @Override
+            public void complete(MessageRenderHandler run) {
+                String uuid = UUID.randomUUID().toString();
+                showMessageHandlers.put(uuid, run);
+                UnityPlayer.UnitySendMessage(gameObject, methodName, uuid);
+            }
+
+            @Override
+            public void error(String error) {
+            }
+        });
+
+    }
+
+    public static void renderMessage(String uuid) {
+        ShowMessageHandler.MessageRenderHandler messageRenderHandler = showMessageHandlers.get(uuid);
+        if (messageRenderHandler != null)
+            messageRenderHandler.render();
+    }
+
+}
