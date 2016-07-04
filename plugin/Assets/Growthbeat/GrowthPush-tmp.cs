497fe96696c5d59918e518f8f695c2edc2e80570
Sota Yamashita <sota.yamashita@gmail.com>
2016-07-04 15:36:57 +0900

    Refactor code

--- plugin/Assets/Growthbeat/GrowthPush-tmp.cs
+++ plugin/Assets/Growthbeat/GrowthPush-tmp.cs
@@ -10,17 +10,17 @@ public class GrowthPush {
   private delete void FailureHandler ();
 
   #if UNITY_IPHONE && !UNITY_EDITOR
-	[DllImport("__Internal")] private static extern void gp_requestDeviceToken();
-	[DllImport("__Internal")] private static extern void gp_setDeviceToken();
+  [DllImport("__Internal")] private static extern void gp_requestDeviceToken();
+  [DllImport("__Internal")] private static extern void gp_setDeviceToken();
   [DllImport("__Internal")] private static extern bool gp_enableNotification();
-	[DllImport("__Internal")] private static extern void gp_clearBadge();
+  [DllImport("__Internal")] private static extern void gp_clearBadge();
   [DllImport("__Internal")] private static extern void gp_setTag(string name);
-	[DllImport("__Internal")] private static extern void gp_setTag(string name, string value);
+  [DllImport("__Internal")] private static extern void gp_setTag(string name, string value);
   [DllImport("__Internal")] private static extern void gp_setTag(GrowthPush.TagType type, string value, string value);
-	[DllImport("__Internal")] private static extern void gp_trackEvent(string name);
+  [DllImport("__Internal")] private static extern void gp_trackEvent(string name);
   [DllImport("__Internal")] private static extern void gp_trackEvent(string name, string value);
   [DllImport("__Internal")] private static extern void gp_trackEvent(string name, string value, ShowMessageHandler showMessageHandler, FailureHandler failureHandler);
-	[DllImport("__Internal")] private static extern void gp_setDeviceTags();
-	[DllImport("__Internal")] private static extern void gp_setBaseUrl(string baseUrl);
-	#endif
+  [DllImport("__Internal")] private static extern void gp_setDeviceTags();
+  [DllImport("__Internal")] private static extern void gp_setBaseUrl(string baseUrl);
+  #endif
 }
--- plugin/Assets/Plugins/iOS/GrowthPush-tmp.mm
+++ plugin/Assets/Plugins/iOS/GrowthPush-tmp.mm
@@ -12,61 +12,56 @@ NSString* GPNSStringFromCharString(const char* charString) {
 
 extern "C" {
 
-  void gp_requestDeviceToken () {
-      [[GrowthPush sharedInstance] requestDeviceToken];
-  }
+	void gp_requestDeviceToken () {
+		[[GrowthPush sharedInstance] requestDeviceToken];
+	}
+	
+	void gp_setDeviceToken(const char* deviceToken) {
+		[[GrowthPush sharedInstance] setDeviceToken:GPNSStringFromCharString(deviceToken)];
+	}
+	
+	bool gp_enableNotification() {
+		[[GrowthPush sharedInstance] enableNotification];
+	}
+	
+	void gp_clearBadge() {
+		[[GrowthPush sharedInstance] clearBadge];
+	}
+	
+	void gp_setTag(const char* name) {
+		[[GrowthPush sharedInstance] setTag:GPNSStringFromCharString(name)];
+	}
+	
+	void gp_setTag(const char* name, const char* value) {
+		[[GrowthPush sharedInstance] setTag:GPNSStringFromCharString(name) value:GPNSStringFromCharString(value)];
+	}
+	
+	void gp_setTag(const GPTagType type, const char* name, const char* value) {
+		[[GrowthPush sharedInstance] setTag:GPNSStringFromCharString(type) name:GPNSStringFromCharString(name), value:GPNSStringFromCharString(value)];
+	}
+	
+	void gp_trackEvent(const char* name) {
+		[[GrowthPush sharedInstance] setTag:GPNSStringFromCharString(name)];
+	}
+	
+	void gp_trackEvent(const char* name, const char* value) {
+		[[GrowthPush sharedInstance] setTag:GPNSStringFromCharString(name) value:GPNSStringFromCharString(value)];
+	}
+	
+	void gp_trackEvent(const char* name, const char* value, ShowMessageHandler showMessageHandler, FailureHandler failureHandler) {
+		[[GrowthPush sharedInstance] setTag:GPNSStringFromCharString(name) value:GPNSStringFromCharString(value) showMessage: ^{ renderMessage(); } failure: ^ (NSString *detail) {}]
+	}
 
-  void gp_setDeviceToken(const char* deviceToken) {
-      [[GrowthPush sharedInstance] setDeviceToken:GPNSStringFromCharString(deviceToken)];
-  }
-
-  bool gp_enableNotification() {
-      [[GrowthPush sharedInstance] enableNotification];
-  }
-
-  void gp_clearBadge() {
-      [[GrowthPush sharedInstance] clearBadge];
-  }
-
-  void gp_setTag(const char* name) {
-      [[GrowthPush sharedInstance] setTag:GPNSStringFromCharString(name)];
-  }
-
-  void gp_setTag(const char* name, const char* value) {
-      [[GrowthPush sharedInstance] setTag:GPNSStringFromCharString(name) value:GPNSStringFromCharString(value)];
-  }
-
-  void gp_setTag(const GPTagType type, const char* name, const char* value) {
-      [[GrowthPush sharedInstance] setTag:GPNSStringFromCharString(type) name:GPNSStringFromCharString(name), value:GPNSStringFromCharString(value)];
-  }
-
-  void gp_trackEvent(const char* name) {
-      [[GrowthPush sharedInstance] setTag:GPNSStringFromCharString(name)];
-  }
-
-  void gp_trackEvent(const char* name, const char* value) {
-      [[GrowthPush sharedInstance] setTag:GPNSStringFromCharString(name) value:GPNSStringFromCharString(value)];
-  }
-
-  void gp_trackEvent(const char* name, const char* value, ShowMessageHandler showMessageHandler, FailureHandler failureHandler) {
-      [[GrowthPush sharedInstance] setTag:GPNSStringFromCharString(name)
-                                   value:GPNSStringFromCharString(value)
-                                   showMessage: ^{
-                                     renderMessage();
-                                   }
-                                   failure: ^ (NSString *detail) {}]
-  }
-
-  void gp_setDeviceTags() {
-      [[GrowthPush sharedInstance] setDeviceTags];
-  }
-
-  void gp_selectButton() {
-      [[GrowthPush sharedInstance] selectButton: ];
-  }
-
-  void gp_notifyClose() {
-      [[GrowthPush sharedInstance] notifyClose];
-  }
+	void gp_setDeviceTags() {
+		[[GrowthPush sharedInstance] setDeviceTags];
+	}
+	
+	void gp_selectButton() {
+		[[GrowthPush sharedInstance] selectButton];
+	}
+	
+	void gp_notifyClose() {
+		[[GrowthPush sharedInstance] notifyClose];
+	}
 
 }
